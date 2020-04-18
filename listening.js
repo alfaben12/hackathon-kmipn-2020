69e55854ef54663ba917a1d81af314443c67cfc0
Thariqalfa <zrav420@gmail.com>
2020-04-18 22:26:07 +0700

    update idea
    pdf to video/slide

---
+++ listening.js
@@ -0,0 +1,120 @@
+const fs = require('fs')
+const pdf = require('@touno-io/pdf')
+const express = require('express')
+var natural = require('natural');
+const app = express()
+const port = 3000
+const GoogleImages = require('google-images');
+const nlp = require('compromise');
+var gis = require('g-i-s');
+var cors = require('cors');
+
+app.use(cors());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	// allow preflight
+	
+	if (req.method === 'OPTIONS') {
+		res.send(200);
+	} else {
+		next();
+	}
+});
+
+const reduceNoun = async (text) => {
+	return await nlp(text).nouns().json()
+}
+
+const blankGenText = async (str, text) => {
+	return await text.toLowerCase().replace(str, '____');
+}
+
+const client = new GoogleImages('009039845094310497023:weh5dj4jjnk', 'AIzaSyB8pYQPY6KLDx7SVreOILwwPYdYreh4SUo');
+
+let dataBuffer = fs.readFileSync('./History_of_Indonesia.pdf')
+Tokenizer = require('nalapa').tokenizer;
+app.use(express.json({limit: '50mb', extended: true}));
+
+app.use(express.urlencoded({ extended: true,limit:'50mb',parameterLimit:500000 }));
+
+function get_google_image(query){
+	return new Promise((resolve, reject) => {
+		client.search(query)
+		.then(images => {
+			resolve(images[0])
+		})
+	})
+}
+
+function get_question(sentence){
+	return new Promise((resolve, reject) => {
+		let qa_array = []
+		reduceNoun(sentence)
+		.then((data) => data.map(async str => {
+			blankGenText(str.text.toLowerCase(), sentence)
+			.then((ques) => qa_array.push({
+				question: ques,
+				answer: str.text
+			}))
+			.catch(err => reject(err));
+		}))
+		.catch(err => reject(err));
+		
+		resolve(qa_array)
+	})
+}
+
+function img(query){
+	return new Promise(resolve => {
+		gis(query, function logResults(error, results) {
+			// console.log(results[0])
+			if (error) {
+				resolve(null)
+			}else{
+				resolve(results[0])
+			}
+		});
+	});
+}
+
+app.post('/', async (req, res) => {
+	let query = req.body.query
+	pdf(dataBuffer).then(async function(data) {
+		data = data.text.replace(/(\r\n|\n|\r)/gm, "")
+		data = data.replace(/ +(?= )/g,'')
+		let paragraph_array = Tokenizer.splitSentence(data)
+		let split_sentence_array = paragraph_array.map(function(paragraph) {
+			natural.PorterStemmer.attach();
+			let split_sentence = Tokenizer.splitSentence(paragraph)
+			return {
+				sentence: paragraph,
+				split_sentence: split_sentence
+			}
+		})
+		
+		let data_result = []
+		for (let i = 0; i < split_sentence_array.length; i++) {
+			let sentence = split_sentence_array[i].sentence;
+			let split_sentence = split_sentence_array[i].split_sentence;
+			let split = split_sentence[0].split(" or ")[0];
+			let sentence_split = sentence.split(",");
+			let image = null
+			// if (i % 3 == 1) {
+				// image = await img(split)
+				// image = await get_google_image(split)
+			// }
+			console.log(i)
+			
+			data_result.push({
+				sentence: sentence_split,
+				split: split,
+				image: image,
+				question: await get_question(sentence)
+			})
+		}
+		return res.json(data_result)
+	})
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ public/text2speech.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Text to Speech</title>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<style>
+.container{
+	width: 100%;
+	height: 600px;
+}
+img{
+	width:100%; 
+	height:100%; 
+	object-fit:contain; 
+}
+.centered {
+	position: absolute;
+	top: 80%;
+	left: 50%;
+	font-size: 25px;
+	transform: translate(-50%, -50%);
+}
+</style>
+</head>
+<body style="background-color:#919191">
+<div class="container">
+<img src="">
+<font class="centered"></font>
+</div>
+</body>
+<script>
+$.ajax({
+	url: 'http://localhost:3000/',
+	type: 'POST',
+	dataType: 'json',
+	cache: false,
+	beforeSend: function() {},
+	success: async function(response) {
+		let all_text = ''
+		// response.map(function(data){
+			for (let i = 0; i < response.length; i++) {
+				// text2speech(data.sentence, data.image)
+				$("img").attr("src", response[i].image.url)
+				
+				for (let j = 0; j < response[i].sentence.length; j++) {
+					$("font").text(response[i].sentence[j])
+					
+					var msg = new SpeechSynthesisUtterance(response[i].sentence[j]);
+					var voices = window.speechSynthesis.getVoices();
+					msg.voice = voices[32];
+					// msg.voice = voices[6];
+					msg.rate = 1;
+					window.speechSynthesis.speak(msg);
+					
+					function delaying() {
+						return new Promise(function(res) {
+							msg.onend = function(event) {
+								return res(event.elapsedTime)
+							}
+						})
+					}
+					await delaying() // delaying
+				}
+				continue
+			}
+			// })
+		}
+	})
+	
+	function timer(ms) {
+		return new Promise(res => setTimeout(res, ms));
+	}
+	
+	function text2speech(text, img){
+		$("img").attr("src", data.image)
+		$("font").text(data.sentence)
+		
+		var msg = new SpeechSynthesisUtterance(text);
+		var voices = window.speechSynthesis.getVoices();
+		msg.voice = voices[32];
+		msg.rate = 1;
+		window.speechSynthesis.speak(msg);
+		
+		msg.onend = function(event) {
+			
+		}
+	}
+	</script>
+	</html>
